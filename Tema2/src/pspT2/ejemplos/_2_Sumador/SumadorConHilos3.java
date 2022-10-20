eb2cf6e2cc432b7b131ab7f573fcc1d5c6e02550
yorhsperez <yorhsprogramacion@gmail.com>
2022-10-20 10:35:04 +0200

    hoy  20/10/22

--- Tema2/src/pspT2/ejemplos/_2_Sumador/SumadorConHilos3.java
+++ Tema2/src/pspT2/ejemplos/_2_Sumador/SumadorConHilos3.java
@@ -3,7 +3,7 @@ package pspT2.ejemplos._2_Sumador;
 class Acumulador {
     long acumulador = 0;
 
-    void acumular(long l) {
+     synchronized public void acumular(long l) {
         acumulador += l;
     }
 
@@ -37,30 +37,29 @@ class HiloSumador3 implements Runnable {
 public class SumadorConHilos3 {
 
     static Acumulador suma;
-    static int MAX_HILOS = 10;
+    static int MAX_HILOS = 200;
 
     public static void main(String[] args) {
+        long TInicio, TFin, tiempo;
+        TInicio = System.currentTimeMillis();
         suma = new Acumulador();
 
-        double n1 = 1.0;
-        double n2 = 100000;
-		double paso = (n2 - n1) / MAX_HILOS;
-//		Thread t1=new Thread(new HiloSumador3(1,33333,suma));
-//		Thread t2=new Thread(new HiloSumador3(33334,66666,suma));
-//		Thread t3=new Thread(new HiloSumador3(66667,100000,suma));
-//		t1.start();
-//		t2.start();
-//		t3.start();
-		//hola
+        long n1 = 1;
+        long n2 = 100000;
+		long paso = (n2 - n1) / MAX_HILOS;
+
+
+
         Thread[] hilos = new Thread[MAX_HILOS];
         for (int i = 0; i < MAX_HILOS; i++) {
             System.out.println("Lanzando hilo desde " + n1 + " a " + (n1 + paso));
             hilos[i] = new Thread(new HiloSumador3((int) n1, (int) (n1 + paso), suma));
             hilos[i].setName("hilo" + (i + 1));
             hilos[i].start();
-            n1 = n1 + paso;
+            n1 = n1 + paso+1;
 
         }
+
 
         try {
             for (int i = 0; i < MAX_HILOS; i++) {
@@ -73,6 +72,11 @@ public class SumadorConHilos3 {
             e.printStackTrace();
         }
         System.out.println("Suma:" + suma.getAcumulador());
+
+        TFin = System.currentTimeMillis();
+        tiempo = TFin - TInicio;
+
+        System.out.println("\nTiempo de ejecución en milisegundos: " + tiempo);
 
     }
 }
