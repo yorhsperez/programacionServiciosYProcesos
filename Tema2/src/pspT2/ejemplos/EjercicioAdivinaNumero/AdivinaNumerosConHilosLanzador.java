c062f2768aec8755be5003e11bbc0b3e817db003
yorhsperez <yorhsprogramacion@gmail.com>
2022-10-21 08:56:54 +0200

    hoy  21/10/22 si

--- Tema2/src/pspT2/ejemplos/EjercicioAdivinaNumero/AdivinaNumerosConHilosLanzador.java
+++ Tema2/src/pspT2/ejemplos/EjercicioAdivinaNumero/AdivinaNumerosConHilosLanzador.java
@@ -6,9 +6,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 
 class Random{
-     public int random;
+     public static int random;
      synchronized public int getRandom(){
-        random = ThreadLocalRandom.current().nextInt(1,100);
+        random = ThreadLocalRandom.current().nextInt(1,1000000);
         return random;
     }
 }
@@ -26,7 +26,7 @@ class AdivinaNumerosConHilos implements Runnable {
         public void run () {
         //El hilo intenta adivinar el numero
             Random random = new Random();
-        int numeroAdivinado = ThreadLocalRandom.current().nextInt(1, 100);
+        int numeroAdivinado = ThreadLocalRandom.current().nextInt(1, 1000000);
         int contador = 0;
 
             System.out.println("Hola soy "+nombre);
@@ -48,7 +48,7 @@ class AdivinaNumerosConHilos implements Runnable {
             if (adivinado==1){
                 System.out.println("He acertado!! soy "+nombre+" y he necesitado "+contador+" intentos");
             }else{
-                System.out.println("Ohhh, no he ganado ("+nombre+")");
+                System.out.println("Ohhh, no he ganado ("+nombre+")"+" lo he intentado "+contador+" intentos");
             }
 
 
@@ -64,18 +64,18 @@ class AdivinaNumerosConHilos implements Runnable {
 
 
 public class AdivinaNumerosConHilosLanzador {
-    //static Random random;
-    static int MAX_HILOS = 20;
+    static Random random;
+    static int MAX_HILOS = 10;
     public static void main(String[] args) {
         long TInicio, TFin, tiempo;
 
         TInicio = System.currentTimeMillis();
         Integer numero;
-//        random= new Random();
-//        numero = random.getRandom();
+        random= new Random();
+        numero = random.getRandom();
 
 
-        System.out.println("El numero a adivinar es: " + 10);
+        System.out.println("El numero a adivinar es: " + numero);
 
         Thread[] hilos = new Thread[MAX_HILOS];
         for (int i = 0; i < MAX_HILOS; i++) {
